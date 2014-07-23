b50d460a3e6beff230e918b9727ef8ec8dd02f74
Ellen Marie Dash <me@duckie.co>
2014-07-23 06:19:10 -0400

    Remove duplicate 'function' keyword.

--- test.rb
+++ test.rb
@@ -1,6 +1,6 @@
 //#<?function puts($a){echo "\x8\x8\x8$a\n";}/*
 //;def main(&b)b&&yield end;"\
-puts=console.log;function
-//"#*/function
+puts=console.log;//*///\
+function//"
 main(){puts("Hello, world!");};//;\
 main();
