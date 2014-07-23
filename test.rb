d5e35856fcb279b12551823ae2c04e44835b236a
Ellen Marie Dash <me@duckie.co>
2014-07-23 06:35:10 -0400

    Cleaned up a bit more.

--- test.rb
+++ test.rb
@@ -1,7 +1,6 @@
 //#<?function puts($a){echo"\x8\x8\x8$a\n";}/*
 //;def main;yield end;"\
-puts=console.log;//*/#\
-function//"
+puts=console.log;/**/function//"
 main(){puts("Hello, world!");}
 //;"\
 main();//"
