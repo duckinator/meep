a57aca14f1902a3ebe7e3fbad70ab1710b9be759
Ellen Marie Dash <me@duckie.co>
2014-07-23 05:43:22 -0400

    w00t :3

--- test.rb
+++ test.rb
@@ -1,34 +1,9 @@
-#/*<?php error_reporting(E_ALL ^ E_NOTICE);
-# */
-#include <stdio.h>
-#include <stdlib.h>
-#define false 0
+//#<?/*
+//;def main;yield end;"\
+puts=console.log;exit=function(){};function
+//"
+main(){puts(
+"*/?>Hello, world!<?/*    ");}
+//;"\
+main()//"#*/?>
 
-# /*
-"
-# */
-int main() {
-# /*
-"; # */
-
-# /*
-if ("00"==false)
-  console.log("Hello from JavaScript!");
-else # */
-if ("0"==false)
-  printf("Hello from PHP!\n");
-else if (0==false)
-  printf("Hello from C!\n");
-else
-  printf("Hello from Ruby!\n");
-
-
-# /*
-end;
-end;
-end;
-# */
-
-#define CBRACKET }
-exit(0);
-CBRACKET;
