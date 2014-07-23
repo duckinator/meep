6b7bd96f1e6cb9fbc47f41302eed0ab3082c2925
Ellen Marie Dash <me@duckie.co>
2014-07-23 06:14:36 -0400

    w00t! no more hard-coded ^H's or significant dangling newlines. :3

--- test.rb
+++ test.rb
@@ -1,9 +1,6 @@
-//#<?/*
-//;def main;yield end;"\
+//#<?function puts($a){echo "\x8\x8\x8$a\n";}/*
+//;def main(&b)b&&yield end;"\
 puts=console.log;function
-//"
-main(){puts(
-"*/?>Hello, world!<?/*    ");}
-//;"\
-main()//"#*/?>
-
+//"#*/function
+main(){puts("Hello, world!");};//;\
+main();
