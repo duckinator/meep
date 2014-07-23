94815ef37e0007ec584e074285fb05865a462eb3
Ellen Marie Dash <me@duckie.co>
2014-07-23 06:30:41 -0400

    Made it a character shorter, yet again. :3

--- test.rb
+++ test.rb
@@ -1,6 +1,7 @@
 //#<?function puts($a){echo"\x8\x8\x8$a\n";}/*
 //;def main;yield end;"\
-puts=console.log;//*///\
+puts=console.log;//*/#\
 function//"
-main(){puts("Hello, world!");};//;"\
+main(){puts("Hello, world!");}
+//;"\
 main();//"
